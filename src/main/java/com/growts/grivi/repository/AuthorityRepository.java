083a20de2f7db92b6509e49a17c435bd9c5b225c
Ricky Vadala <rickyvadala@gmail.com>
2021-03-18 19:48:19 -0300

    Initial version of grivi generated by JHipster-6.10.3 with blueprints: vuejs-1.9.2

---
+++ src/main/java/com/growts/grivi/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.growts.grivi.repository;
+
+import com.growts.grivi.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
